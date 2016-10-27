2941f2456f52e5d9c45e3fa129052bf20cad10a7
Andrew T. Kim <andrewtevinkim@gmail.com>
2016-10-27 17:04:33 -0400

    1st commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pernilla::Application.config.secret_key_base = 'c61478b9fc33f742c7cb691358074907509a73f680fd6549da4cdee2dd31dd6a276363ccd798f8e7ff0ccf4cb17b518acddcb1e3355e729835618e97e57b18dc'

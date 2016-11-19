8da9a37d4eaba69630df9a4d8f71cfb6d9ef9665
Andrew T. Kim <andrewtevinkim@gmail.com>
2016-11-19 03:00:33 -0500

    Commit single file sends

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,2 +1,9 @@
 class StaticPagesController < ApplicationController
+
+  def send_mp3
+    if !user_signed_in? || !current_user.admin?
+      raise "Go away, you evil villain!"
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,13 @@ Pernilla::Application.routes.draw do
   devise_for :users
   root 'static_pages#home'
 
-  get '/send', to: 'static_pages#send'
+  get '/send_mp3', to: 'static_pages#send_mp3'
+
+  resources :uploads do
+    collection do
+      post 'upload'
+    end
+  end
 
   resources :api, path: :api
   
